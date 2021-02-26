8e3e5c9a2397ba769cc301f5608144bbe2abc7ba
chanlon hoo <chanlonhoo@chanlondeMacBook-Air.local>
2021-02-26 10:14:22 +0800

    as

--- docs/index/index.js
+++ docs/index/index.js
@@ -27,19 +27,19 @@ new Docute({
         children: [
           {
             title: '项目概述与项目经理',
-            link: '/program/pm-gaishu'
+            link: './program/pm-gaishu'
           },
           {
             title: '项目组结构及阶段发展',
-            link: '/program/xiangmuzhu'
+            link: './program/xiangmuzhu'
           },
           {
             title: '人才资源管理',
-            link: '/program/hrm'
+            link: './program/hrm'
           },
           {
             title: '沟通管理',
-            link: '/program/goutong'
+            link: './program/goutong'
           }
         ]
       },
@@ -48,11 +48,11 @@ new Docute({
         children: [
           {
             title: '项目范围过程',
-            link: '/program/fanwei'
+            link: './program/fanwei'
           },
           {
             title: '项目进度预估及管控',
-            link: '/program/time-manager'
+            link: './program/time-manager'
           }
         ]
       },
@@ -61,27 +61,27 @@ new Docute({
         children: [
         {
             title: '采购管理',
-            link: '/program/caigou'
+            link: './program/caigou'
           },
           {
             title: '投入成本',
-            link: '/program/cost'
+            link: './program/cost'
           },
           {
             title: '风险概论',
-            link: '/program/fengxian'
+            link: './program/fengxian'
           },
           {
             title: '风险对策',
-            link: '/program/duice'
+            link: './program/duice'
           },
           {
             title: '质量管理',
-            link: '/program/zhiliang'
+            link: './program/zhiliang'
           },
           {
             title: '项目收尾管理',
-            link: '/program/shouwei'
+            link: './program/shouwei'
           }
         ]
       },
@@ -90,23 +90,23 @@ new Docute({
         children: [
           {
             title: '桌维驻场服务季度报告',
-            link: '/bid/ji-du-bao-gao'
+            link: './bid/ji-du-bao-gao'
           },
           {
             title: '岗位手册编写格式',
-            link: '/bid/gan-wei-man-gs'
+            link: './bid/gan-wei-man-gs'
           },
           {
             title: '甲方公司岗位工作手册',
-            link: '/bid/gan-wei-man'
+            link: './bid/gan-wei-man'
           },
           {
             title: '桌维项目标书编写格式',
-            link: '/bid/tou-biao-shu-ge-shi'
+            link: './bid/tou-biao-shu-ge-shi'
           },
           {
             title: '甲方代维项目服务（投标书）',
-            link: '/bid/tou-biao-shu'
+            link: './bid/tou-biao-shu'
           }
         ]
       }
